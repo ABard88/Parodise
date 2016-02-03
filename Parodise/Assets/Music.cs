4b083a3fe02e7c22f1b1fc47a31b2cc671603683
Akshay Bardia <akbdjk@gmail.com>
2016-02-02 17:28:05 -0800

    Sound plays through without stop. And quip message.

---
+++ Parodise/Assets/Music.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour 
+{
+
+	void Start()
+	{
+	}
+	void Update()
+	{
+	}
+
+	public static Music Instance;
+	void Awake()
+	{
+		if (Instance) {
+			DestroyImmediate (gameObject);
+		} 
+		else // continue playing music from where it left off
+		{
+			DontDestroyOnLoad (gameObject);
+			Instance=this;
+		}
+	}
+	void OnLevelWasLoaded(int level) // in these levels there is different music so stop playing old one
+	{
+		if(level==2 || level==3 || level==4)
+		{
+			DestroyImmediate (gameObject);
+		}
+	}
+
+}
