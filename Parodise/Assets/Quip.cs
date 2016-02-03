0135670065f0ead520ef2642434d5dc508edbe45
Akshay Bardia <akbdjk@gmail.com>
2016-02-02 17:57:13 -0800

    Continuous music and quip label.

---
+++ Parodise/Assets/Quip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quip : MonoBehaviour 
+{
+	public GameObject quip;
+
+	void OnMouseOver()
+	{
+		quip.transform.localPosition = new Vector3 (0, -3, 0);
+	}
+	void OnMouseExit()
+	{
+		quip.transform.localPosition = new Vector3 (1000, 0, 0);	
+	}
+}
